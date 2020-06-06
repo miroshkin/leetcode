b314bf39da91cf7cbf51e599edc95b8eed60d014
miroshkin <anton.miroshkin@gmail.com>
2020-06-06 09:07:09 +0300

    Added ReturnDuplicatesFromSortedArray answer

--- Leetcode/Leetcode/EasyCollection/ReturnDuplicatesFromSortedArray.cs
+++ Leetcode/Leetcode/EasyCollection/ReturnDuplicatesFromSortedArray.cs
@@ -11,11 +11,38 @@ namespace Leetcode.EasyCollection
         public void Answer()
         {
             Console.WriteLine($"Array length is {RemoveDuplicates(nums)}");
+            PrintOutArray();
+        }
+
+        private void PrintOutArray()
+        {
+            string arrayContent = string.Empty;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                arrayContent += ", " + nums[i].ToString();
+            }
+            Console.WriteLine($"[{arrayContent}]");
         }
 
         public int RemoveDuplicates(int[] nums)
         {
-
+            //Checks the initial array length
+            //If the array is empty function returns 0
+            if (nums.Length == 0) return 0;
+            
+            //There is one element in array at least
+            int i = 0;
+            
+            //Loop starts from the second array element
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[j] != nums[i])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+            return i + 1;
         }
     }
 }
