cbb85fdb5bfd60f236a7c807823cf140eb2ff193
miroshkin <anton.miroshkin@gmail.com>
2020-06-17 07:46:40 +0300

    Added tree folder with first task

---
+++ Leetcode/Leetcode/EasyCollection/Trees/MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.EasyCollection.Trees
+{
+    //https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/555/
+    class MaximumDepthOfBinaryTree : IQuestion
+    {
+        void IQuestion.Answer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
